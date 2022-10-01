fe254e6352c26303aad6250b5af3f8cce983b3c0
Onomeh Onwuanibe <60798978+Estherstarr@users.noreply.github.com>
2022-10-01 11:43:44 +0000

    Revert "Setup association"

--- capitalize_decorator.rb
+++
@@ -1,8 +0,0 @@
-require_relative 'base_decorator'
-
-# Defines a capitalize_decorator class
-class CapitalizeDecorator < BaseDecorator
-  def correct_name
-    @nameable.correct_name.capitalize
-  end
-end
