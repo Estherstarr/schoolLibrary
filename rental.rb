d03d8e2177ca9580327113b03dda5e3eecac70d9
onomeh <onwuanibeonome@gmail.com>
2022-10-01 01:44:23 +0000

    Create rental class with association

---
+++ rental.rb
@@ -0,0 +1,21 @@
+# Defines a Rental class
+class Rental
+  attr_accessor :date
+  attr_reader :book, :person
+
+  def initialize(date:, book:, person:)
+    @date = date
+    @book = book
+    @person = person
+  end
+
+  def person=(person)
+    @person = person
+    person.rentals.push(self) unless person.rentals.include?(self)
+  end
+
+  def book=(book)
+    @book = book
+    book.rentals.push(self) unless book.rentals.include?(self)
+  end
+end
