6f4a899fee252a5b20c9e2c20745c8426081c36a
onomeh <onwuanibeonome@gmail.com>
2022-10-05 16:38:32 +0000

    Update student

---
+++ book.rb
@@ -0,0 +1,23 @@
+# Defines a Book class
+class Book
+  attr_accessor :title, :author
+  attr_reader :rentals
+
+  def initialize(title:, author:)
+    @title = title
+    @author = author
+    @rentals = []
+  end
+
+  def add_rental(rental)
+    @rentals << rental
+    rental.book = self
+  end
+  
+  def to_h
+    {
+      title: title,
+      author: author
+    }
+  end
+end
